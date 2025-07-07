c721c50a06b9377f53c3281f55a01d294d65bb8a
Elder Millenial X <x@Elders-Air.lan>
2025-07-07 12:46:02 -0400

    basic calculator - for loop

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,12 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut result:u32 = 1;
+
+    for i in 0..n {
+       result += i; 
+    }
+
+    return result;
 }
 
 #[cfg(test)]
